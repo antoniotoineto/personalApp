2bb43ec2835f9160966216edb408a3eb7edd7098
antoniotoineto <netocaastro10@gmail.com>
2024-08-21 15:18:15 -0300

    Adicionando configuração para armazenamento em memória cache

--- src/app/context/InvestimentosContext.tsx
+++ src/app/context/InvestimentosContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useState, useContext, ReactNode } from 'react';
+import React, { createContext, useState, useContext, useEffect, ReactNode } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface InvestimentosContextProps {
   investimentos: number;
@@ -8,10 +9,35 @@ interface InvestimentosContextProps {
 const InvestimentosContext = createContext<InvestimentosContextProps | undefined>(undefined);
 
 export const InvestimentosProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [investimentos, setInvestimentos] = useState<number>(0);
+  const [investimentos, setInvestimentosState] = useState<number>(0);
+
+  useEffect(() => {
+    const loadInvestimentos = async () => {
+      try {
+        const storedInvestimentos = await AsyncStorage.getItem('@investimentos');
+        if (storedInvestimentos !== null) {
+          setInvestimentosState(parseFloat(storedInvestimentos));
+        }
+      } catch (e) {
+        console.error("Erro ao carregar 'investimentos' do Armazenamento em Cache", e);
+      }
+    };
+
+    loadInvestimentos();
+  }, []);
+
+  const saveInvestimentos = async (value: number) => {
+    try {
+      await AsyncStorage.setItem('@investimentos', value.toString());
+      setInvestimentosState(value);
+      console.log("'Investimentos' salvo em memória cache!")
+;    } catch (e) {
+      console.error("Erro ao salvar 'investimentos' no Armazenamento em Cache", e);
+    }
+  };
 
   return (
-    <InvestimentosContext.Provider value={{ investimentos, setInvestimentos }}>
+    <InvestimentosContext.Provider value={{ investimentos, setInvestimentos: saveInvestimentos }}>
       {children}
     </InvestimentosContext.Provider>
   );
